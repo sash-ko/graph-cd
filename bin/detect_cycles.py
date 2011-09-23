6c1680d2fe29962bef966e3678137c68f9502853
Oleksandr Lysenko <sashkolysenko@gmail.com>
2011-09-23 12:40:29 +0300

    fix output

--- bin/detect_cycles.py
+++ bin/detect_cycles.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python
 
+import sys
 import argparse
 from pygraph.readwrite.dot import read
 try:
     from cycles.detector import detect
 except:
-    import sys
     sys.path.append('')
     from cycles.detector import detect
 
 def main(args):
     with open(args.file) as f:
         g = read(''.join(f.readlines()))
-    detect(g)
+    compoments = detect(g)
+    if args.output:
+        with open(args.output, 'w') as f:
+            f.write(str(compoments))
+    else:
+        sys.stdout.write(str(compoments))
 
 if __name__ == '__main__':
 
